b91804e8f628c34f7b1491d717d33f8813e0f6c6
Halister Fernando <halisterfernando@hotmail.com>
2023-05-03 16:56:29 -0300

    feat: criada pagina home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React, {useContext} from 'react';
+import FormContainer from '../components/styles/FormContainer'
+import Form from '../components/Form';
+import Card from '../components/Card';
+import { CardContext } from '../context/CardProvider';
+import CardPreview from '../components/styles/PreviewStyles';
+import Header from '../components/Header';
+import Rules from '../components/Rules';
+import Footer from '../components/Footer';
+
+export default function Home() {
+    const { card } = useContext(CardContext)
+  return (
+    <>
+        <Rules />
+        <Header />
+        <FormContainer>
+            <Form />
+            <CardPreview>
+                <Card 
+                    name={card.name}
+                    rarity={card.rarity}
+                    image={card.image}
+                    description={card.description}
+                    speed={card.attributes.velocidade}
+                    weight={card.attributes.peso}
+                    length={card.attributes.comprimento}
+                    />
+            </CardPreview>
+        </FormContainer>
+        <Footer />
+    </>
+  )
+}
