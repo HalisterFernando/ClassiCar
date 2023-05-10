b02e4bc525e959e30c7d13cde0186aa455d9e03d
Halister Fernando <halisterfernando@hotmail.com>
2023-05-09 23:24:01 -0300

    tests: testes para cadastro de cartas, refatorado teste de filtro por raridade

--- src/test/home.test.js
+++ src/test/home.test.js
@@ -63,6 +63,90 @@ describe('Home', () => {
 
         expect(newCard).toBeInTheDocument;     
     });
+    it('should not be able to create a card without a name', () => {
+        renderWithRouter(<App />);
+        
+        const description = screen.getByTestId('card-description');
+        const speed = screen.getByTestId('speed');
+        const weight = screen.getByTestId('weight');
+        const length = screen.getByTestId('length');
+        const image = screen.getByTestId('card-image')
+        const saveBtn = screen.getByTestId('save-btn');
+      
+        userEvent.type(description, 'Famoso Volkswagen');
+        expect(description).toHaveValue('Famoso Volkswagen');
+       
+        userEvent.type(speed, '133');
+        expect(speed).toHaveValue(133);
+       
+        userEvent.type(weight, '780');
+        expect(weight).toHaveValue(780);        
+       
+        userEvent.type(length, '4');
+        expect(length).toHaveValue(4);
+
+        userEvent.type(image, 'fusca.jpg');
+        expect(image).toHaveValue('fusca.jpg');      
+       
+
+
+        expect(saveBtn).toHaveAttribute('disabled');
+     
+    });
+    it('should not be able to create a card without a description', () => {
+        renderWithRouter(<App />);
+
+        const name = screen.getByTestId('card-name');        
+        const speed = screen.getByTestId('speed');
+        const weight = screen.getByTestId('weight');
+        const length = screen.getByTestId('length');
+        const image = screen.getByTestId('card-image')
+        const saveBtn = screen.getByTestId('save-btn');              
+
+        userEvent.type(name, 'Fusca');
+        expect(name).toHaveValue('Fusca');      
+       
+        userEvent.type(speed, '133');
+        expect(speed).toHaveValue(133);
+       
+        userEvent.type(weight, '780');
+        expect(weight).toHaveValue(780);        
+       
+        userEvent.type(length, '4');
+        expect(length).toHaveValue(4);
+
+        userEvent.type(image, 'fusca.jpg');
+        expect(image).toHaveValue('fusca.jpg');      
+
+        expect(saveBtn).toHaveAttribute('disabled');
+    });
+    it('should not be able to create a card without an image', () => {
+        renderWithRouter(<App />);
+
+        const name = screen.getByTestId('card-name');
+        const description = screen.getByTestId('card-description');
+        const speed = screen.getByTestId('speed');
+        const weight = screen.getByTestId('weight');
+        const length = screen.getByTestId('length');       
+        const saveBtn = screen.getByTestId('save-btn');              
+
+        userEvent.type(name, 'Fusca');
+        expect(name).toHaveValue('Fusca');
+       
+        userEvent.type(description, 'Famoso Volkswagen');
+        expect(description).toHaveValue('Famoso Volkswagen');
+       
+        userEvent.type(speed, '133');
+        expect(speed).toHaveValue(133);
+       
+        userEvent.type(weight, '780');
+        expect(weight).toHaveValue(780);        
+       
+        userEvent.type(length, '4');
+        expect(length).toHaveValue(4);
+
+        expect(saveBtn).toHaveAttribute('disabled');
+    });
     it('should be able to delete a card', () => {
         renderWithRouter(<App />)
 
@@ -72,7 +156,7 @@ describe('Home', () => {
 
         expect(deleteCard).not.toBeInTheDocument
     });
-    it('should be able to create a super trumph card is no one exists', () => {
+    it('should be able to create a super trumph card if no one exists', () => {
         renderWithRouter(<App />)
 
         const superTrumphCard = screen.getByTestId('delete-btn-Fusca');
@@ -81,7 +165,7 @@ describe('Home', () => {
 
         expect(superTrumph).not.toBeInTheDocument;
         
-        for (const badge of superTrumphBadges ) {
+        for (const badge of superTrumphBadges) {
             expect(badge).not.toBeInTheDocument;
             
         }
@@ -138,10 +222,17 @@ describe('Home', () => {
             return styles            
         }
         
-        checkCardRarity(normal).every((bg) => expect(bg).toBe('rgb(243, 243, 243)'));
-        checkCardRarity(raro).every((bg) => expect(bg).toBe('silver'));
-        checkCardRarity(muitoRaro).every((bg) => expect(bg).toBe('gold'));
+        for (const backgroundColor of checkCardRarity(normal)) {
+            expect(backgroundColor).toBe('rgb(243, 243, 243)')
+        }
 
+        for (const backgroundColor of checkCardRarity(raro)) {
+            expect(backgroundColor).toBe('silver')
+        }
+
+        for (const backgroundColor of checkCardRarity(muitoRaro)) {
+            expect(backgroundColor).toBe('gold')
+        }
     });
     it('should be able to start game after clicking ok', async () => {
         const { history } = renderWithRouter(<App />);
